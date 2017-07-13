b54f000fb0e67736e06c1723e499b60aae9c62ee
Dennis <dennis@tryitonline.net>
2017-07-13 02:25:56 +0000

    improved tiows

--- lib/tiocache.py
+++ lib/tiocache.py
@@ -7,24 +7,24 @@ from threading import Lock
 from time import time
 
 class Cache:
-	_magic = b'\x4C\x83\x82\x69\xC8\x5E'
-	_reserved = b'\x00\x00'
+	__magic = b'\x4C\x83\x82\x69\xC8\x5E'
+	__reserved = b'\x00\x00'
 
-	def _drop(self, key):
-		value = self._dict.pop(key)
-		self._mem_inner -= asizeof(key) + asizeof(value)
+	def __drop(self, key):
+		value = self.__dict.pop(key)
+		self.__mem_inner -= asizeof(key) + asizeof(value)
 
-	def _trim(self):
-		self._lock.acquire()
+	def __trim(self):
+		self.__lock.acquire()
 		try:
-			if self.get_mem_usage() > self._mem_upper:
-				for key in sorted(self._dict, key = self.get_atime):
-					self._drop(key)
-					if self.get_mem_usage() <= self._mem_lower:
+			if self.get_mem_usage() > self.__mem_upper:
+				for key in sorted(self.__dict, key = self.get_atime):
+					self.__drop(key)
+					if self.get_mem_usage() <= self.__mem_lower:
 						break
-			self._lock.release()
+			self.__lock.release()
 		except:
-			self._lock.release()
+			self.__lock.release()
 			raise
 
 	@classmethod
@@ -35,42 +35,42 @@ class Cache:
 		keylen, mem_lower, mem_upper, mem_inner = unpack('<QQQQ', file.read(32))
 		sep = file.read(8)
 		cache = cls(keylen, mem_lower, mem_upper)
-		cache._mem_inner = mem_inner
-		cache._dict = {
+		cache.__mem_inner = mem_inner
+		cache.__dict = {
 			chunk[:keylen]: [*unpack('<d', chunk[keylen : keylen + 8]), chunk[keylen + 8 :]]
 			for chunk in file.read().split(sep)
 		}
 		return cache
 
 	def get_atime(self, key):
-		return self._dict[key][0]
+		return self.__dict[key][0]
 
 	def get_mem_usage(self):
-		return getsizeof(self._dict) + self._mem_inner
+		return getsizeof(self.__dict) + self.__mem_inner
 
 	def save_to(self, filename):
-		self._lock.acquire()
+		self.__lock.acquire()
 		try:
 			tempfile = '{}.{:8x}'.format(filename, getrandbits(64))
 			file = open(tempfile, 'wb')
 			while True:
 				sep = pack('<Q', getrandbits(64))
 				dump = \
-					self._magic + \
-					self._reserved + \
-					pack('<QQQQ', self._keylength, self._mem_lower, self._mem_upper, self._mem_inner) + \
+					self.__magic + \
+					self.__reserved + \
+					pack('<QQQQ', self.__keylength, self.__mem_lower, self.__mem_upper, self.__mem_inner) + \
 					sep + \
-					sep.join(key + pack('d', value[0]) + value[1] for key, value in self._dict.items())
-				if dump.count(sep) == max(len(self._dict), 1):
+					sep.join(key + pack('d', value[0]) + value[1] for key, value in self.__dict.items())
+				if dump.count(sep) == max(len(self.__dict), 1):
 					break
 			file.write(dump)
 			file.flush()
 			fsync(file.fileno())
 			file.close()
 			rename(tempfile, filename)
-			self._lock.release()
+			self.__lock.release()
 		except:
-			self._lock.release()
+			self.__lock.release()
 			try:
 				remove(tempfile)
 			except FileNotFoundError:
@@ -79,59 +79,59 @@ class Cache:
 
 	def validate_key(self, key):
 		assert type(key) == bytes
-		assert len(key) == self._keylength
+		assert len(key) == self.__keylength
 
 	def __delitem__(self, key):
 		self.validate_key(key)
-		self._lock.acquire()
+		self.__lock.acquire()
 		try:
-			self._drop(key)
-			self._lock.release()
+			self.__drop(key)
+			self.__lock.release()
 		except:
-			self._lock.release()
+			self.__lock.release()
 			raise
 
 	def __init__(self, keylength, mem_lower, mem_upper):
-		self._dict = {}
-		self._lock = Lock()
-		self._keylength = keylength
-		self._mem_lower = mem_lower
-		self._mem_upper = mem_upper
-		assert self._mem_lower <= self._mem_upper
-		self._mem_inner = asizeof(self) - getsizeof(self._dict)
+		self.__dict = {}
+		self.__lock = Lock()
+		self.__keylength = keylength
+		self.__mem_lower = mem_lower
+		self.__mem_upper = mem_upper
+		assert self.__mem_lower <= self.__mem_upper
+		self.__mem_inner = asizeof(self) - getsizeof(self.__dict)
 
 	def __iter__(self):
-		return iter(self._dict)
+		return iter(self.__dict)
 
 	def __len__(self):
-		return len(self._dict)
+		return len(self.__dict)
 
 	def __getitem__(self, key):
 		self.validate_key(key)
-		self._lock.acquire()
+		self.__lock.acquire()
 		try:
-			value = self._dict[key]
+			value = self.__dict[key]
 			value[0] = time()
-			self._lock.release()
+			self.__lock.release()
 		except:
-			self._lock.release()
+			self.__lock.release()
 			raise
 		return value[1]
 
 	def __setitem__(self, key, value):
 		self.validate_key(key)
 		value = [time(), value]
-		self._lock.acquire()
+		self.__lock.acquire()
 		try:
-			if key in self._dict:
-				self._mem_inner -= asizeof(self._dict[key])
-				self._dict[key] = value
-				self._mem_inner += asizeof(value)
+			if key in self.__dict:
+				self.__mem_inner -= asizeof(self.__dict[key])
+				self.__dict[key] = value
+				self.__mem_inner += asizeof(value)
 			else:
-				self._dict[key] = value
-				self._mem_inner += asizeof(key) + asizeof(value)
-			self._lock.release()
+				self.__dict[key] = value
+				self.__mem_inner += asizeof(key) + asizeof(value)
+			self.__lock.release()
 		except:
-			self._lock.release()
+			self.__lock.release()
 			raise
-		self._trim()
+		self.__trim()
--- lib/tiows.py
+++ lib/tiows.py
@@ -1,4 +1,6 @@
-import asyncio, asyncssh, hashlib, random, ssl, threading, tiocache, websockets, zlib
+import asyncio, asyncssh, hashlib, random, socket, threading, tiocache, traceback, websockets, zlib
+
+cache_file = '/srv/var/cache/tiows'
 
 def _counter():
 	counter = 0
@@ -11,49 +13,74 @@ counter = _counter()
 async def auto_save():
 	while True:
 		await asyncio.sleep(600)
-		cache.save_to('/srv/var/cache/tiows')
+		cache.save_to(cache_file)
 
 async def connect(hostname):
 	connections[hostname] = await asyncssh.connect(hostname, username = 'runner', known_hosts = '/etc/ssh/ssh_known_hosts')
 
 async def get_environment(handler_id):
 	lock.acquire()
-	if handler_id in hostnames:
-		hostname = hostnames[handler_id]
-		context = contexts[handler_id]
-		active = True
-	else:
-		hostname = min(pool, key = lambda hostname: pool[hostname] + random.random())
-		pool[hostname] += 1
-		while True:
-			context = 's0-s0:c{},c{},c{},c{}'.format(*sorted(random.sample(range(1024), 4)))
-			if context not in contexts.values():
-				break
-		hostnames[handler_id] = hostname
-		contexts[handler_id] = context
-		active = False
-	if not hostname in connections:
-		await connect(hostname)
-	lock.release()
-	return (hostname, context, active)
+	try:
+		if handler_id in hostnames:
+			hostname = hostnames[handler_id]
+			context = contexts[handler_id]
+			active = True
+		else:
+			hostname = min(pool, key = lambda hostname: pool[hostname] + random.random())
+			if not hostname in connections:
+				await connect(hostname)
+			while True:
+				context = 's0-s0:c{},c{},c{},c{}'.format(*sorted(random.sample(range(1024), 4)))
+				if context not in contexts.values():
+					break
+			hostnames[handler_id] = hostname
+			contexts[handler_id] = context
+			pool[hostname] += 1
+			active = False
+		lock.release()
+		return (hostname, context, active)
+	except socket.gaierror:
+		pool[hostname] = 1 << 32
+		print('Disabled arena "{}".'.format(hostname))
+		lock.release()
+		raise
+	except:
+		lock.release()
+		raise
 
 async def release_environment(handler_id):
 	lock.acquire()
-	hostname = hostnames[handler_id]
-	pool[hostname] -= 1
-	del contexts[handler_id]
-	del hostnames[handler_id]
+	try:
+		hostname = hostnames[handler_id]
+	except KeyError:
+		lock.release()
+		return
+	try:
+		pool[hostname] -= 1
+	except KeyError:
+		pass
+	try:
+		del contexts[handler_id]
+	except KeyError:
+		pass
+	try:
+		del hostnames[handler_id]
+	except KeyError:
+		pass
 	lock.release()
+
+async def send_error(websocket, message):
+	await websocket.send(zlib.compress(br'/o\ /o\ /o\ /o\ ' + message.encode())[2:-4])
 
 connections = {}
 contexts = {}
 hostnames = {}
 lock = threading.Lock()
-pool = {'tio1': 0, 'tio2': 0}
+pool = {'tio1': 0, 'tio2': 0, 'tio3': 0}
 
 async def consumer(websocket, handler_id):
-	try:
-		while True:
+	while True:
+		try:
 			message = await websocket.recv()
 			if message[:4] == 'PROB':
 				key = int(message[4:], 16).to_bytes(32, 'big')
@@ -65,7 +92,10 @@ async def consumer(websocket, handler_id):
 			hostname, context, active = await get_environment(handler_id)
 			connection = connections[hostname]
 			if active:
-				await connection.run('killall -Z {0} -INT; sleep 1; killall -Z {0} -KILL'.format(context))
+				try:
+					await connection.run('killall -Z {0} -INT; sleep 1; killall -Z {0} -KILL'.format(context))
+				except:
+					pass
 				hostname, context, active = await get_environment(handler_id)
 			if message == 'QUIT':
 				await release_environment(handler_id)
@@ -77,13 +107,19 @@ async def consumer(websocket, handler_id):
 			await websocket.send(response)
 			await release_environment(handler_id)
 			cache[hashlib.sha256(message).digest()] = response
-	except websockets.exceptions.ConnectionClosed:
-		pass
+		except websockets.exceptions.ConnectionClosed:
+			break
+		except zlib.error:
+			await release_environment(handler_id)
+			await send_error(websocket, 'The server could not understand the request.')
+		except:
+			await release_environment(handler_id)
+			traceback.print_exc(limit = 1)
+			await send_error(websocket, 'Internal server error. Please try again.')
 
 async def handler(websocket, _):
-	ip, port = websocket.remote_address
 	handler_id = next(counter)
-	print('{:012x} {} {}:{}'.format(handler_id, 'CONNECT', ip, port))
+	print('{:012x} {} {}'.format(handler_id, 'CONNECT', websocket.request_headers.get('X-Forwarded-For')))
 	_, pending = await asyncio.wait(
 		[asyncio.ensure_future(consumer(websocket, handler_id)) for _ in range(2)],
 		return_when = asyncio.FIRST_COMPLETED
@@ -92,19 +128,15 @@ async def handler(websocket, _):
 		task.cancel()
 	print('{:012x} {}'.format(handler_id, 'CLOSE'))
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_cert_chain('/etc/letsencrypt/live/tio.run/fullchain.pem', '/etc/letsencrypt/live/tio.run/privkey.pem')
-start_server = websockets.serve(handler, 'tio.run', 8080, ssl = context)
-
 try:
-	cache = tiocache.Cache.load_from('/srv/var/cache/tiows')
+	cache = tiocache.Cache.load_from(cache_file)
 except:
 	cache = tiocache.Cache(32, 120 << 20, 128 << 20)
 
 try:
 	asyncio.ensure_future(auto_save())
-	asyncio.get_event_loop().run_until_complete(start_server)
+	asyncio.get_event_loop().run_until_complete(websockets.serve(handler, 'localhost', 410))
 	asyncio.get_event_loop().run_forever()
 except:
-	cache.save_to('/srv/var/cache/tiows')
+	cache.save_to(cache_file)
 	raise
